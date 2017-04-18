06df28b1292534d5e23d1611ea61ecd4726ea0b7
Natalie Nix <natalie.nix@aggiemail.usu.edu>
2017-04-18 22:14:37 +0000

    CAN edit posts.

--- blog/urls.py
+++ blog/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'), 
     
     # ex: /blog/5/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
