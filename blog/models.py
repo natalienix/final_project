4d720c9885abf56fa75b8de0b1c56b5f78827c4c
Natalie Nix <natalie.nix@aggiemail.usu.edu>
2017-04-18 20:54:14 +0000

    Changed Model and registered with admin.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
-
+from django.utils import timezone
 from django.db import models
 
 
@@ -17,3 +17,18 @@ class Choice(models.Model):
     
     def __str__(self):
         return self.choice_text
+        
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
