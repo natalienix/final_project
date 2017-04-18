fc5dcf4ffbae9586bf6a2f33a7e7380a409dfd7c
Natalie Nix <natalie.nix@aggiemail.usu.edu>
2017-04-18 22:13:41 +0000

    Can not edit posts.

--- blog/urls.py
+++ blog/urls.py
@@ -2,14 +2,15 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'blog'
+#app_name = 'blog'
 urlpatterns = [
     # ex: /blog/
     #url(r'^$', views.index, name='index'),
     
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    #url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     
     # ex: /blog/5/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
