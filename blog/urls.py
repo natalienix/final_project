a5a5d61bfcd73c747879e3754cea40223e614bb6
Natalie Nix <natalie.nix@aggiemail.usu.edu>
2017-04-19 02:04:27 +0000

    Pages linked now. CSS better.

--- blog/urls.py
+++ blog/urls.py
@@ -11,14 +11,16 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'), 
-    url(r'^detail/$', views.index, name='index'),
+    url(r'^index/$', views.index, name='index'),
     # ex: /blog/5/
-    url(r'^post/(?P<pk>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     
-    # ex: /blog/5/results/
+    # ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     
-    # ex: /blog/5/vote/
-    url(r'^(?P<pk>\d+)/vote/$', views.vote, name='vote'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    
+    
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -11,16 +11,16 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'blog/index.html', context)
 
-def detail(request, pk):
-    question = get_object_or_404(Question, pk)
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'blog/detail.html', {'question': question})
 
-def results(request, pk):
-    question = get_object_or_404(Question, pk)
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'blog/results.html', {'question': question})
 
-def vote(request, pk):
-    question = get_object_or_404(Question, pk)
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
@@ -35,7 +35,7 @@ def vote(request, pk):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('blog:results', args=(pk,)))
+        return HttpResponseRedirect(reverse('results', args=(question.id,)))
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
