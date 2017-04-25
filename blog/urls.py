39e02c723981ce9b2f5edfb7540f5ee3488f5439
Natalie Nix <natalie.nix@aggiemail.usu.edu>
2017-04-25 17:08:31 +0000

    Spacing changes.

--- blog/urls.py
+++ blog/urls.py
@@ -4,23 +4,13 @@ from . import views
 
 #app_name = 'blog'
 urlpatterns = [
-    # ex: /blog/
-    
-    
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'), 
     url(r'^feedback/$', views.feedback, name='feedback'),
-    # ex: /blog/5/
     url(r'^feedback/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    
-    # ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    
-    # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
-    
-    
 ]
 
