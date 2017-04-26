e235b1822ca1866526446e5f68d77a00c4f50ac1
Natalie Nix <natalie.nix@aggiemail.usu.edu>
2017-04-26 17:02:07 +0000

    Added Javascript element.

--- blog/static/blog/javascript.js
+++ blog/static/blog/javascript.js
@@ -4,14 +4,6 @@ function getConfirmation(){
     }
 }
 
-// interact('.resizable')
-//   .resizable({
-//     edges: { right: true, bottom: true }
-//   })
-//   .on('resizemove', function (event) {
-//     var target = event.target;
-
-//     // update the element's style
-//     target.style.width  = event.rect.width + 'px';
-//     target.style.height = event.rect.height + 'px';
-//   });
+function postSaved(){
+    alert('Post Saved!');
+}
